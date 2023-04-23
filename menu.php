628ff5aee62aec8e77b91dbb785a24feb29e2215
alwaystiredbtw <eduardomoura2@hotmail.com>
2023-04-23 18:38:15 -0300

    ...

--- menu.php
+++ menu.php
@@ -1,26 +1,29 @@
 <header>
-    <nav class="navigation">
-        <img src="src/img/logo.jpeg">
-        <a href="index.php" class="logo">Whey <span>Market</span></a>
-        <ul class="nav-menu">
-            <i class='bx bx-search'></i>
-            <li class="nav-item"><a href="index.php">Home</a></li>
-            <li class="nav-item"><a href="#">Produtos</a></li>
-            <li class="nav-item"><a href="#">Carrinho</a></li>
-            <li class="nav-item"><a href="#">Contato</a></li>
-            <?php
-            if(!empty($_SESSION) && isset($_SESSION['logado']) && $_SESSION['logado'] == false):
-            ?>
-            <li class="nav-item"><a href="login.php">Login/Cadastro</a></li>
-            <?php
-            else:
-            ?>
-            <li class="nav-item"><span class='text-red'>Ola, <?php echo $_SESSION['Nome']?></span></li>
-            <li class="nav-item"><a href="logout.php">Sair</a></li>
-            <?php
-            endif;
-            ?>
-        </ul>
+    <nav class="navigation ">
+        <div class="flex-center-logo">
+            <img src="src/img/logo.jpg" width="100px" height="100px">
+            <a href="index.php" class="logo">Whey <span>Market</span></a>
+        </div>
+        <div>
+            <ul class="nav-menu">
+                <i class='bx bx-search'></i>
+                <li class="nav-item"><a href="index.php">Home</a></li>
+                <li class="nav-item"><a href="#">Produtos</a></li>
+                <li class="nav-item"><a href="#">Carrinho</a></li>
+                <li class="nav-item"><a href="#">Contato</a></li>
+                <?php
+                if(!empty($_SESSION) && isset($_SESSION['logado']) && $_SESSION['logado'] == false):
+                ?>
+                <li class="nav-item"><a href="login.php">Login/Cadastro</a></li>
+                <?php
+                else:
+                ?>
+                <li class="nav-item"><a href="logout.php">Sair</a></li>
+                <?php
+                endif;
+                ?>
+            </ul>
+        </div>
         <div class="menu">
             <span class="bar"></span>
             <span class="bar"></span>
