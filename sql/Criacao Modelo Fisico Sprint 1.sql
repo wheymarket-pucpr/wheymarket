c5e5f25ae1a11d7d515a5e7205da2210832c620b
Leonardo <leoklein_pr@hotmail.com>
2023-04-25 08:52:31 -0300

    banco de dados atualizado

--- sql/Criacao Modelo Fisico Sprint 1.sql
+++ sql/Criacao Modelo Fisico Sprint 1.sql
@@ -1,54 +1,71 @@
-/* Modelo Fisico Sprint 1: Criação das tabelas e Povoamento*/
 CREATE TABLE Lojista (
-    ID INTEGER PRIMARY KEY,
+    ID INT PRIMARY KEY AUTO_INCREMENT,
     CNPJ CHAR(14) UNIQUE,
-    email VARCHAR(30),
-    Nome VARCHAR(100)
+    email VARCHAR(50)UNIQUE,
+    Nome VARCHAR(100),
+	senha CHAR(32),
+    fk_Cadastro_Tipo_ID INT
 );
 
 CREATE TABLE Produto (
     SKU CHAR(5),
-    fk_Lojista_ID INTEGER,
-    fk_Categoria_Produto_ID INTEGER,
+    fk_Lojista_ID INT,
+    fk_Categoria_Produto_ID INT,
     Nome VARCHAR(100),
     Preco FLOAT,
-    Quantidade INTEGER,
+    Quantidade INT,
     Peso FLOAT,
     Foto BLOB,
     Descricao VARCHAR(1000),
-    PRIMARY KEY (SKU, fk_Lojista_ID)
+    PRIMARY KEY (fk_Lojista_ID, SKU)
 );
 
 CREATE TABLE Categoria_Produto (
-    ID INTEGER PRIMARY KEY,
+    ID INT PRIMARY KEY AUTO_INCREMENT,
+    Nome VARCHAR(30)
+);
+
+
+CREATE TABLE Cadastro_Tipo (
+    ID INT PRIMARY KEY AUTO_INCREMENT,
     Nome VARCHAR(30)
 );
  
-ALTER TABLE Produto ADD CONSTRAINT FK_Produto_1
+ALTER TABLE Lojista ADD CONSTRAINT FK_Lojista_2
+    FOREIGN KEY (fk_Cadastro_Tipo_ID)
+    REFERENCES Cadastro_Tipo (ID)
+    ON DELETE CASCADE;
+ 
+ALTER TABLE Produto ADD CONSTRAINT FK_Produto_2
     FOREIGN KEY (fk_Categoria_Produto_ID)
     REFERENCES Categoria_Produto (ID)
     ON DELETE CASCADE;
  
-ALTER TABLE Produto ADD CONSTRAINT FK_Produto_2
+ALTER TABLE Produto ADD CONSTRAINT FK_Produto_3
     FOREIGN KEY (fk_Lojista_ID)
     REFERENCES Lojista (ID)
     ON DELETE CASCADE;
     
-    
-insert into lojista (id, nome, email, cnpj) values (1, "Fernando Souza", "fernando@gmail.com", "15625548000147");
-insert into lojista (id, nome, email, cnpj) values (2, "Flavia lopes", "flavia@gmail.com", "85425548000102");
-insert into lojista (id, nome, email, cnpj) values (3, "Maria oliveira", "maria.com", "65487458000165");
-insert into lojista (id, nome, email, cnpj) values (4, "Pedro alcantara", "pedro@gmail.com", "15632236000178");
-insert into lojista (id, nome, email, cnpj) values (5, "Julio lins", "julio@gmail.com", "8915753000157");
+insert into Cadastro_tipo (nome) values 
+("Lojista"), ("Consumidor"), ("Administrador");
 
 
- insert into Categoria_Produto(id, nome) values (1, "Termogênicos");
- insert into Categoria_Produto(id, nome) values (2, "Aminoácidos");
- insert into Categoria_Produto(id, nome) values (3, "Acessórios");
- 
- 
-insert into produto(sku, fk_Lojista_ID, nome, preco, quantidade, peso, fk_Categoria_Produto_ID) values("25fa", 1, "Creatina", 150.00, 85, 500.00, 2);
-insert into produto(sku, fk_Lojista_ID, nome, preco, quantidade, peso, fk_Categoria_Produto_ID) values("38fd", 2, "Glutamina", 75.00, 50, 250.00, 2);
-insert into produto(sku, fk_Lojista_ID, nome, preco, quantidade, peso, fk_Categoria_Produto_ID) values("df10", 3, "Cafeína em Pó", 50.00, 100, 80.00, 1);
-insert into produto(sku, fk_Lojista_ID, nome, preco, quantidade, peso, fk_Categoria_Produto_ID) values("e23f", 4, "Coqueteleira", 70.00, 150, NULL, 3);
-insert into produto(sku, fk_Lojista_ID, nome, preco, quantidade, peso, fk_Categoria_Produto_ID) values("85d63", 5, "Strep", 30.00, 200, NULL, 3);
+insert into Categoria_Produto(nome) values 
+("Termogênicos"), ("Aminoácidos"), ("Acessórios");
+
+# senha : teste123
+
+insert into lojista (nome, email, cnpj, senha, fk_Cadastro_Tipo_ID) values 
+("Fernando Souza", "fernando@gmail.com", "15625548000147", "aa1bf4646de67fd9086cf6c79007026c", 1),
+("Flavia lopes", "flavia@gmail.com", "85425548000102", "aa1bf4646de67fd9086cf6c79007026c", 1),
+("Maria oliveira", "maria.com", "65487458000165", "aa1bf4646de67fd9086cf6c79007026c", 1),
+("Pedro alcantara", "pedro@gmail.com", "15632236000178", "aa1bf4646de67fd9086cf6c79007026c", 1),
+("Julio lins", "julio@gmail.com", "89157530001578", "aa1bf4646de67fd9086cf6c79007026c", 1);
+
+
+insert into produto(sku, fk_Lojista_ID, nome, preco, quantidade, peso, fk_Categoria_Produto_ID) values
+("25fa", 1, "Creatina", 150.00, 85, 500.00, 2),
+("38fd", 2, "Glutamina", 75.00, 50, 250.00, 2),
+("df10", 3, "Cafeína em Pó", 50.00, 100, 80.00, 1),
+("e23f", 4, "Coqueteleira", 70.00, 150, NULL, 3),
+("85d63", 5, "Strep", 30.00, 200, NULL, 3);
