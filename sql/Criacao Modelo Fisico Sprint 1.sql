fc12a8ca3b2d17ff3003381c11dc5706d4eddebb
Eduardo Moura <107185485+alwaystiredbtw@users.noreply.github.com>
2023-04-29 16:18:10 -0300

    bdwheymarket

--- sql/Criacao Modelo Fisico Sprint 1.sql
+++ sql/Criacao Modelo Fisico Sprint 1.sql
@@ -129,7 +129,7 @@ CREATE TABLE `fotos` (
   `nome` varchar(100) DEFAULT NULL,
   `path` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
