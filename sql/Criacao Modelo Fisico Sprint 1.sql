b961948c2d423c59ff0dae37d2a49507f32c2570
gilmardenck88 <gilmardenck88@gmail.com>
2023-04-29 15:51:57 -0300

    nco de dados

--- sql/Criacao Modelo Fisico Sprint 1.sql
+++ sql/Criacao Modelo Fisico Sprint 1.sql
@@ -1,259 +1,222 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
+-- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
 --
--- Host: 127.0.0.1
--- Tempo de geração: 29/04/2023 às 17:15
--- Versão do servidor: 10.4.28-MariaDB
--- Versão do PHP: 8.2.4
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: bdwheymarket
+-- ------------------------------------------------------
+-- Server version	8.0.32
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Banco de dados: `bdwheymarket`
+-- Table structure for table `administrador`
 --
 
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `administrador`
---
-
+DROP TABLE IF EXISTS `administrador`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `administrador` (
-  `id` int(11) NOT NULL,
-  `email` varchar(140) NOT NULL,
-  `senha` varchar(32) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `id` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(140) COLLATE utf8mb4_general_ci NOT NULL,
+  `senha` varchar(32) COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Despejando dados para a tabela `administrador`
+-- Dumping data for table `administrador`
 --
 
-INSERT INTO `administrador` (`id`, `email`, `senha`) VALUES
-(1, 'admin@admin', '21232f297a57a5a743894a0e4a801fc3');
-
--- --------------------------------------------------------
+LOCK TABLES `administrador` WRITE;
+/*!40000 ALTER TABLE `administrador` DISABLE KEYS */;
+INSERT INTO `administrador` VALUES (1,'admin@admin','21232f297a57a5a743894a0e4a801fc3');
+/*!40000 ALTER TABLE `administrador` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Estrutura para tabela `cadastro_tipo`
+-- Table structure for table `cadastro_tipo`
 --
 
+DROP TABLE IF EXISTS `cadastro_tipo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `cadastro_tipo` (
-  `ID` int(11) NOT NULL,
-  `Nome` varchar(30) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `Nome` varchar(30) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Despejando dados para a tabela `cadastro_tipo`
+-- Dumping data for table `cadastro_tipo`
 --
 
-INSERT INTO `cadastro_tipo` (`ID`, `Nome`) VALUES
-(1, 'Lojista'),
-(2, 'Consumidor'),
-(3, 'Administrador');
-
--- --------------------------------------------------------
+LOCK TABLES `cadastro_tipo` WRITE;
+/*!40000 ALTER TABLE `cadastro_tipo` DISABLE KEYS */;
+INSERT INTO `cadastro_tipo` VALUES (1,'Lojista'),(2,'Consumidor'),(3,'Administrador');
+/*!40000 ALTER TABLE `cadastro_tipo` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Estrutura para tabela `categoria_produto`
+-- Table structure for table `categoria_produto`
 --
 
+DROP TABLE IF EXISTS `categoria_produto`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `categoria_produto` (
-  `ID` int(11) NOT NULL,
-  `Nome` varchar(30) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `Nome` varchar(30) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Despejando dados para a tabela `categoria_produto`
+-- Dumping data for table `categoria_produto`
 --
 
-INSERT INTO `categoria_produto` (`ID`, `Nome`) VALUES
-(1, 'Termogênicos'),
-(2, 'Aminoácidos'),
-(3, 'Acessórios');
-
--- --------------------------------------------------------
+LOCK TABLES `categoria_produto` WRITE;
+/*!40000 ALTER TABLE `categoria_produto` DISABLE KEYS */;
+INSERT INTO `categoria_produto` VALUES (1,'Termogênicos'),(2,'Aminoácidos'),(3,'Acessórios');
+/*!40000 ALTER TABLE `categoria_produto` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Estrutura para tabela `consumidor`
+-- Table structure for table `consumidor`
 --
 
+DROP TABLE IF EXISTS `consumidor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `consumidor` (
-  `id` int(11) NOT NULL,
-  `Nome` varchar(100) NOT NULL,
-  `CPF` varchar(20) NOT NULL,
-  `senha` char(32) NOT NULL,
-  `email` varchar(100) NOT NULL
+  `id` int NOT NULL AUTO_INCREMENT,
+  `Nome` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `CPF` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
+  `senha` char(32) COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `id_cadastro_tipo` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_cadastro_tipo` (`id_cadastro_tipo`),
+  CONSTRAINT `consumidor_ibfk_1` FOREIGN KEY (`id_cadastro_tipo`) REFERENCES `cadastro_tipo` (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Despejando dados para a tabela `consumidor`
+-- Dumping data for table `consumidor`
 --
 
-INSERT INTO `consumidor` (`id`, `Nome`, `CPF`, `senha`, `email`) VALUES
-(1, 'usuario teste', '00397898965', '81dc9bdb52d04dc20036dbd8313ed055', 'novo@usuario.com');
-
--- --------------------------------------------------------
+LOCK TABLES `consumidor` WRITE;
+/*!40000 ALTER TABLE `consumidor` DISABLE KEYS */;
+/*!40000 ALTER TABLE `consumidor` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Estrutura para tabela `lojista`
+-- Table structure for table `fotos`
 --
 
+DROP TABLE IF EXISTS `fotos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `fotos` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) DEFAULT NULL,
+  `path` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `fotos`
+--
+
+LOCK TABLES `fotos` WRITE;
+/*!40000 ALTER TABLE `fotos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `fotos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `lojista`
+--
+
+DROP TABLE IF EXISTS `lojista`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `lojista` (
-  `ID` int(11) NOT NULL,
-  `CNPJ` char(14) DEFAULT NULL,
-  `email` varchar(50) DEFAULT NULL,
-  `Nome` varchar(100) DEFAULT NULL,
-  `senha` char(32) DEFAULT NULL,
-  `fk_Cadastro_Tipo_ID` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `CNPJ` char(14) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `email` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `Nome` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `senha` char(32) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `fk_Cadastro_Tipo_ID` int DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `CNPJ` (`CNPJ`),
+  UNIQUE KEY `email` (`email`),
+  KEY `FK_Lojista_2` (`fk_Cadastro_Tipo_ID`),
+  CONSTRAINT `FK_Lojista_2` FOREIGN KEY (`fk_Cadastro_Tipo_ID`) REFERENCES `cadastro_tipo` (`ID`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Despejando dados para a tabela `lojista`
+-- Dumping data for table `lojista`
 --
 
-INSERT INTO `lojista` (`ID`, `CNPJ`, `email`, `Nome`, `senha`, `fk_Cadastro_Tipo_ID`) VALUES
-(1, '15625548000147', 'fernando@gmail.com', 'Fernando Souza', 'aa1bf4646de67fd9086cf6c79007026c', 1),
-(2, '85425548000102', 'flavia@gmail.com', 'Flavia lopes', 'aa1bf4646de67fd9086cf6c79007026c', 1),
-(3, '65487458000165', 'maria@gmail.com', 'Maria oliveira', 'aa1bf4646de67fd9086cf6c79007026c', 1),
-(4, '15632236000178', 'pedro@gmail.com', 'Pedro alcantara', 'aa1bf4646de67fd9086cf6c79007026c', 1),
-(5, '8915753000157', 'julio@gmail.com', 'Julio lins', 'aa1bf4646de67fd9086cf6c79007026c', 1),
-(10, '12345678901234', 'eduardo@moura.com', 'Loja CSGO', 'aa1bf4646de67fd9086cf6c79007026c', 1),
-(19, '12345634523414', 'nova@loja.com', 'Loja teste', '81dc9bdb52d04dc20036dbd8313ed055', 1);
-
--- --------------------------------------------------------
+LOCK TABLES `lojista` WRITE;
+/*!40000 ALTER TABLE `lojista` DISABLE KEYS */;
+INSERT INTO `lojista` VALUES (1,'15625548000147','fernando@gmail.com','Fernando Souza','aa1bf4646de67fd9086cf6c79007026c',1),(2,'85425548000102','flavia@gmail.com','Flavia lopes','aa1bf4646de67fd9086cf6c79007026c',1),(3,'65487458000165','maria@gmail.com','Maria oliveira','aa1bf4646de67fd9086cf6c79007026c',1),(4,'15632236000178','pedro@gmail.com','Pedro alcantara','aa1bf4646de67fd9086cf6c79007026c',1),(5,'8915753000157','julio@gmail.com','Julio lins','aa1bf4646de67fd9086cf6c79007026c',1),(10,'12345678901234','eduardo@moura.com','Loja CSGO','aa1bf4646de67fd9086cf6c79007026c',1),(19,'12345634523414','nova@loja.com','Loja teste','81dc9bdb52d04dc20036dbd8313ed055',1);
+/*!40000 ALTER TABLE `lojista` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Estrutura para tabela `produto`
+-- Table structure for table `produto`
 --
 
+DROP TABLE IF EXISTS `produto`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `produto` (
-  `SKU` char(5) NOT NULL,
-  `fk_Lojista_ID` int(11) NOT NULL,
-  `fk_Categoria_Produto_ID` int(11) DEFAULT NULL,
-  `Nome` varchar(100) DEFAULT NULL,
+  `SKU` char(5) COLLATE utf8mb4_general_ci NOT NULL,
+  `fk_Lojista_ID` int NOT NULL,
+  `fk_Categoria_Produto_ID` int DEFAULT NULL,
+  `Nome` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `Preco` float DEFAULT NULL,
-  `Quantidade` int(11) DEFAULT NULL,
+  `Quantidade` int DEFAULT NULL,
   `Peso` float DEFAULT NULL,
-  `Foto` blob DEFAULT NULL,
-  `Descricao` varchar(1000) DEFAULT NULL
+  `Foto` blob,
+  `Descricao` varchar(1000) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `id_foto` int DEFAULT NULL,
+  PRIMARY KEY (`fk_Lojista_ID`,`SKU`),
+  KEY `FK_Produto_2` (`fk_Categoria_Produto_ID`),
+  KEY `id_foto` (`id_foto`),
+  CONSTRAINT `FK_Produto_2` FOREIGN KEY (`fk_Categoria_Produto_ID`) REFERENCES `categoria_produto` (`ID`) ON DELETE CASCADE,
+  CONSTRAINT `FK_Produto_3` FOREIGN KEY (`fk_Lojista_ID`) REFERENCES `lojista` (`ID`) ON DELETE CASCADE,
+  CONSTRAINT `produto_ibfk_1` FOREIGN KEY (`id_foto`) REFERENCES `fotos` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Despejando dados para a tabela `produto`
+-- Dumping data for table `produto`
 --
 
-INSERT INTO `produto` (`SKU`, `fk_Lojista_ID`, `fk_Categoria_Produto_ID`, `Nome`, `Preco`, `Quantidade`, `Peso`, `Foto`, `Descricao`) VALUES
-('25fa', 1, 2, 'Creatina', 150, 85, 500, NULL, NULL),
-('38fd', 2, 2, 'Glutamina', 75, 50, 250, NULL, NULL),
-('df10', 3, 1, 'Cafeína em Pó', 50, 100, 80, NULL, NULL),
-('e23f', 4, 3, 'Coqueteleira', 70, 150, NULL, NULL, NULL),
-('85d63', 5, 3, 'Strep', 30, 200, NULL, NULL, NULL);
+LOCK TABLES `produto` WRITE;
+/*!40000 ALTER TABLE `produto` DISABLE KEYS */;
+INSERT INTO `produto` VALUES ('25fa',1,2,'Creatina',150,85,500,NULL,NULL,NULL),('38fd',2,2,'Glutamina',75,50,250,NULL,NULL,NULL),('df10',3,1,'Cafeína em Pó',50,100,80,NULL,NULL,NULL),('e23f',4,3,'Coqueteleira',70,150,NULL,NULL,NULL,NULL),('85d63',5,3,'Strep',30,200,NULL,NULL,NULL,NULL);
+/*!40000 ALTER TABLE `produto` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Índices para tabelas despejadas
---
-
---
--- Índices de tabela `administrador`
---
-ALTER TABLE `administrador`
-  ADD PRIMARY KEY (`id`);
-
---
--- Índices de tabela `cadastro_tipo`
---
-ALTER TABLE `cadastro_tipo`
-  ADD PRIMARY KEY (`ID`);
-
---
--- Índices de tabela `categoria_produto`
---
-ALTER TABLE `categoria_produto`
-  ADD PRIMARY KEY (`ID`);
-
---
--- Índices de tabela `consumidor`
---
-ALTER TABLE `consumidor`
-  ADD PRIMARY KEY (`id`);
-
---
--- Índices de tabela `lojista`
---
-ALTER TABLE `lojista`
-  ADD PRIMARY KEY (`ID`),
-  ADD UNIQUE KEY `CNPJ` (`CNPJ`),
-  ADD UNIQUE KEY `email` (`email`),
-  ADD KEY `FK_Lojista_2` (`fk_Cadastro_Tipo_ID`);
-
---
--- Índices de tabela `produto`
---
-ALTER TABLE `produto`
-  ADD PRIMARY KEY (`fk_Lojista_ID`,`SKU`),
-  ADD KEY `FK_Produto_2` (`fk_Categoria_Produto_ID`);
-
---
--- AUTO_INCREMENT para tabelas despejadas
---
-
---
--- AUTO_INCREMENT de tabela `administrador`
---
-ALTER TABLE `administrador`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT de tabela `cadastro_tipo`
---
-ALTER TABLE `cadastro_tipo`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT de tabela `categoria_produto`
---
-ALTER TABLE `categoria_produto`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT de tabela `consumidor`
---
-ALTER TABLE `consumidor`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT de tabela `lojista`
---
-ALTER TABLE `lojista`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
-
---
--- Restrições para tabelas despejadas
---
-
---
--- Restrições para tabelas `lojista`
---
-ALTER TABLE `lojista`
-  ADD CONSTRAINT `FK_Lojista_2` FOREIGN KEY (`fk_Cadastro_Tipo_ID`) REFERENCES `cadastro_tipo` (`ID`) ON DELETE CASCADE;
-
---
--- Restrições para tabelas `produto`
---
-ALTER TABLE `produto`
-  ADD CONSTRAINT `FK_Produto_2` FOREIGN KEY (`fk_Categoria_Produto_ID`) REFERENCES `categoria_produto` (`ID`) ON DELETE CASCADE,
-  ADD CONSTRAINT `FK_Produto_3` FOREIGN KEY (`fk_Lojista_ID`) REFERENCES `lojista` (`ID`) ON DELETE CASCADE;
-COMMIT;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-04-29 15:48:37
