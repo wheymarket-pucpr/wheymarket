1058c229d8a5bc5e04f5d600c2329c7ec8a7eacf
Leonardo <leoklein_pr@hotmail.com>
2023-05-08 13:56:19 -0300

    Delete Lojista

---
+++ lojistaDelete.php
@@ -0,0 +1,20 @@
+<?php
+if(!empty($_GET['id']))
+    {
+        include_once('conexao.php');
+
+        $id = $_GET['id'];
+
+        $sqlSelect = "SELECT *  FROM lojista WHERE ID=$id";
+
+        $result = $conn->query($sqlSelect);
+
+        if($result->num_rows > 0)
+        {
+            $sqlDelete = "DELETE FROM lojista WHERE ID=$id";
+            $resultDelete = $conn->query($sqlDelete);
+        }
+    }
+    // Colocar logout e redirecionar para index
+
+?>
