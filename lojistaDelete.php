79253f54c436542b67f5bf300c80b65009de8fcc
eliasfernandescout <eliasfernandescout@gmail.com>
2023-05-09 20:08:04 -0300

    modal de exclusao lojista

--- lojistaDelete.php
+++ lojistaDelete.php
@@ -1,21 +1,19 @@
 <?php
-if(!empty($_GET['id']))
-    {
-        include_once('conexao.php');
+if (!empty($_GET['id'])) {
+    include_once('conexao.php');
 
-        $id = $_GET['id'];
+    $id = $_GET['id'];
 
-        $sqlSelect = "SELECT *  FROM lojista WHERE ID=$id";
+    $sqlSelect = "SELECT *  FROM lojista WHERE ID=$id";
 
-        $result = $conn->query($sqlSelect);
+    $result = $conn->query($sqlSelect);
 
-        if($result->num_rows > 0)
-        {
-            $sqlDelete = "DELETE FROM lojista WHERE ID=$id";
-            $resultDelete = $conn->query($sqlDelete);
-        }
+    if ($result->num_rows > 0) {
+        $sqlDelete = "DELETE FROM lojista WHERE ID=$id";
+        $resultDelete = $conn->query($sqlDelete);
     }
-    require('logout.php');
-    header('location: index.php');
+}
+require('logout.php');
+header('location: index.php');
 
 ?>
--- lojistaPage.php
+++ lojistaPage.php
@@ -20,103 +20,118 @@ if ($result = $conn->query($sql)) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js"
+            integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE"
+            crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js"
+            integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ"
+            crossorigin="anonymous"></script>
     <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="css/style.css">
     <title>WheyMarket</title>
-    <link rel="icon" href="https://cdn0.iconfinder.com/data/icons/fitness-filled/64/fitness-08-512.png" type="image/x-icon">
+    <link rel="icon" href="https://cdn0.iconfinder.com/data/icons/fitness-filled/64/fitness-08-512.png"
+          type="image/x-icon">
 </head>
 
 <body>
-    <header>
-        <nav class="navigation ">
-            <div class="flex-center-logo">
-                <a href="lojistaPage.php"><img src="src/img/logo.png" width="100px" height="100px"></a>
-                <a href="lojistaPage.php" class="logo">Whey <span>Market</span>
-                    <h6>Area do vendedor</h6>
-                </a>
+<header>
+    <nav class="navigation ">
+        <div class="flex-center-logo">
+            <a href="lojistaPage.php"><img src="src/img/logo.png" width="100px" height="100px"></a>
+            <a href="lojistaPage.php" class="logo">Whey <span>Market</span>
+                <h6>Area do vendedor</h6>
+            </a>
 
-            </div>
-            <div>
-                <ul class="nav-menu">
-                    <i class='bx bx-search'></i>
-                    <li class="nav-item"><a href="lojistaPage.php">Inicio</a></li>
-                    <li class="nav-item"><a href="#">Minhas vendas</a></li>
-                    <li class="nav-item"><a href="listarProdutos.php">Meus produtos</a></li>
-                    <li class="nav-item"><a href="cadastroProduto.php">Cadastrar produto</a></li>
-                    <?php
-                    ?>
-                    <li class="nav-item"><span class='text-red'>Ola, <?php echo $_SESSION['Nome'] ?></span></li>
-                    <li class="nav-item"><a href="logout.php">Sair</a></li>
-                    <?php
-                    ?>
-                </ul>
-            </div>
-            <div class="menu">
-                <span class="bar"></span>
-                <span class="bar"></span>
-                <span class="bar"></span>
-            </div>
-        </nav>
-    </header>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col p-5">
-                <table class="table table-hover table-striped">
-                    <thead>
-                        <h2>Dados do Lojista</h2>
-                        <tr>
-                            <th scope="col">Nome</th>
-                            <th scope="col">Email</th>
-                            <th scope="col">CNPJ</th>
-                            <th scope="col">Qtd Produtos cadastrados</th>
-                            <th scope="col">Editar/Deletar</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td><?php echo $lojista['Nome'] ?></td>
-                            <td><?php echo $lojista['email'] ?></td>
-                            <td><?php echo $lojista['CNPJ'] ?></td>
-                            <td><?php echo mysqli_num_rows($result2) ?></td>
-                            <td>
+        </div>
+        <div>
+            <ul class="nav-menu">
+                <i class='bx bx-search'></i>
+                <li class="nav-item"><a href="lojistaPage.php">Inicio</a></li>
+                <li class="nav-item"><a href="#">Minhas vendas</a></li>
+                <li class="nav-item"><a href="listarProdutos.php">Meus produtos</a></li>
+                <li class="nav-item"><a href="cadastroProduto.php">Cadastrar produto</a></li>
+                <?php
+                ?>
+                <li class="nav-item"><span class='text-red'>Ola, <?php echo $_SESSION['Nome'] ?></span></li>
+                <li class="nav-item"><a href="logout.php">Sair</a></li>
+                <?php
+                ?>
+            </ul>
+        </div>
+        <div class="menu">
+            <span class="bar"></span>
+            <span class="bar"></span>
+            <span class="bar"></span>
+        </div>
+    </nav>
+</header>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col p-5">
+            <table class="table table-hover table-striped">
+                <thead>
+                <h2>Dados do Lojista</h2>
+                <tr>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">CNPJ</th>
+                    <th scope="col">Qtd Produtos cadastrados</th>
+                    <th scope="col">Editar/Deletar</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td><?php echo $lojista['Nome'] ?></td>
+                    <td><?php echo $lojista['email'] ?></td>
+                    <td><?php echo $lojista['CNPJ'] ?></td>
+                    <td><?php echo mysqli_num_rows($result2) ?></td>
+                    <td>
 
-                                <!-- botao editar -->
-                                <a class='btn btn-sm btn-primary' href='editLojista.php?&id=<?php echo $lojista['ID'] ?>' title='Editar'>
-                                    <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-pencil' viewBox='0 0 16 16'>
-                                        <path d='M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z' />
-                                    </svg></a>
-                                    <!-- botao deletar -->
-
-                                    <!-- modal teste-->
-                                    <button class="btn btn-sm btn-danger" id="btn-confirm">                                        <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-trash-fill' viewBox='0 0 16 16'>
-                                            <path d='M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z' />
-                                        </svg></button>
-                                    <!--  -->
-                                    <!-- </a>
-                                    <a class='btn btn-sm btn-danger' href='lojistaDelete.php?id=<?php echo $lojista['ID'] ?>' title='Deletar' confirm="Tem certeza que deseja excluir o cadastro?">
-                                        <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-trash-fill' viewBox='0 0 16 16'>
-                                            <path d='M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z' />
-                                        </svg>
-                                    </a> -->
-                                    <div class="modal fade" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true" id="mi-modal">
-                                        <div class="modal-dialog modal-sm">
-                                            <div class="modal-content">
-                                                <div class="modal-header">
-                                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                                                    <h4 class="modal-title" id="myModalLabel">Deletar conta?</h4>
-                                                </div>
-                                                <div class="modal-footer">
-                                                    <button type="button" class="btn btn-default" id="modal-btn-si">Sim</button>
-                                                    <button type="button" class="btn btn-primary" id="modal-btn-no">Nao</button>
-                                                </div>
-                                            </div>
-                                        </div>
+                        <!-- botao editar -->
+                        <a class='btn btn-sm btn-primary' href='editLojista.php?&id=<?php echo $lojista['ID'] ?>'
+                           title='Editar'>
+                            <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor'
+                                 class='bi bi-pencil' viewBox='0 0 16 16'>
+                                <path
+                                    d='M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z'/>
+                            </svg>
+                        </a>
+                        <!-- botao deletar -->
+                        <button type="button" class="btn btn-sm btn-danger" id="btn-confirm" data-bs-toggle="modal"
+                                data-bs-target="#JanelaModal">
+                            <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor'
+                                 class='bi bi-trash-fill' viewBox='0 0 16 16'>
+                                <path
+                                    d='M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z'/>
+                            </svg>
+                        </button>
+                        </a>
+                        <div id="JanelaModal" class="modal fade">
+                            <div class="modal-dialog modal-dialog-scrollable">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h4 class="modal-title">Tem certeza que deseja excluir seu cadastro?</h4>
+<!--                                        <button type="button" class="btn btn-close" data-bs-dismiss="modal"></button>-->
                                     </div>
 
-                                    <div class="alert" role="alert" id="result"></div>
+                                    <div class="modal-body">
+                                        <p>Ao excluir seu cadastro não será mais possível recuperar a conta.</p>
+
+                                    </div>
+
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-danger">
+                                            <a href='lojistaDelete.php?id=<?php echo $lojista['ID'] ?>'>Excluir</a>
+                                        </button>
+
+                                        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Fechar
+                                        </button>
+
+                                    </div>
+                                </div>
+
 </body>
 </td>
 </tr>
@@ -132,28 +147,28 @@ if ($result = $conn->query($sql)) {
 </html>
 
 <script>
-var modalConfirm = function(callback){
-  
-  $("#btn-confirm").on("click", function(){
-    $("#mi-modal").modal('show');
-  });
+    var modalConfirm = function (callback) {
 
-  $("#modal-btn-si").on("click", function(){
-    callback(true);
-    $("#mi-modal").modal('hide');
-  });
-  
-  $("#modal-btn-no").on("click", function(){
-    callback(false);
-    $("#mi-modal").modal('hide');
-  });
-};
+        $("#btn-confirm").on("click", function () {
+            $("#mi-modal").modal('show');
+        });
 
-modalConfirm(function(confirm){
-  if(confirm){
-    //Acciones si el usuario confirma
-    $("#result").html("Deletado!");
-  }
+        $("#modal-btn-si").on("click", function () {
+            callback(true);
+            $("#mi-modal").modal('hide');
+        });
 
-});
+        $("#modal-btn-no").on("click", function () {
+            callback(false);
+            $("#mi-modal").modal('hide');
+        });
+    };
+
+    modalConfirm(function (confirm) {
+        if (confirm) {
+            //Acciones si el usuario confirma
+            $("#result").html("Deletado!");
+        }
+
+    });
 </script>
