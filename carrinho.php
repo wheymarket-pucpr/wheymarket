76d75dfe64b05f6d3e1a09abbdb80bbe22ab1523
eliasfernandescout <eliasfernandescout@gmail.com>
2023-05-25 13:41:51 -0300

    update carrinho

--- carrinho.php
+++ carrinho.php
@@ -13,7 +13,45 @@ session_start();
     ?>
 </header>
 <body>
-<h1>Hello World!</h1>
+<main>
+    <div class="page-title">
+        Seu Carrinho
+    </div>
+    <div class="content">
+        <section>
+            <table>
+                <thead>
+                <tr>
+                    <th>
+                        Produto
+                    </th>
+                    <th>
+                        Preço
+                    </th>
+                    <th>
+                        Quantidade
+                    </th>
+                    <th>
+                        Total
+                    </th>
+                    <th>
+                        -
+                    </th>
+                </tr>
+                </thead>
+                <tbody>
+                <td></td>
+                <td>R$ 120,00</td>
+                <td></td>
+                <td>R$ 240,00</td>
+                <td></td>
+                </tbody>
+            </table>
+        </section>
+
+        <aside>Resumo das compras</aside>
+    </div>
+</main>
 </body>
 </html>
 
