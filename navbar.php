4b2b6c6d11c5c69ab511a62920795300925dacc0
Eduardo Moura <107185485+alwaystiredbtw@users.noreply.github.com>
2023-05-30 19:09:36 -0300

    nova navbar responsiva

--- navbar.php
+++ navbar.php
@@ -1,75 +1,62 @@
 <!DOCTYPE html>
-<html lang="Pt-br">
-<?php include('htmlhead.php') ?>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Exemplo de Interface Responsiva com Bootstrap 5</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container">
+                <a class="navbar-brand" href="index.php">
+                    <img src="src/img/logo.png" alt="Logo" width="100" height="100">
+                    Whey Market
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item">
+                            <form class="d-flex" action="produtos.php" method="POST">
+                                <input class="form-control me-2" type="text" name="busca" placeholder="Pesquisar...">
+                                <button class="btn btn-primary" type="submit">
+                                    <i class='bx bx-search'></i>
+                                </button>
+                            </form>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="produtos.php">Produtos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="carrinho.php">Carrinho</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="lojistaLogin.php">Área do Lojista</a>
+                        </li>
+                        <?php if (!empty($_SESSION) && isset($_SESSION['Nome'])) : ?>
+                            <li class="nav-item">
+                                <span class="nav-link text-danger">Olá, <?php echo $_SESSION['Nome'] ?></span>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="logout.php">Sair</a>
+                            </li>
+                        <?php else : ?>
+                            <li class="nav-item">
+                                <a class="nav-link" href="consumidorLogin.php">Login/Cadastro</a>
+                            </li>
+                        <?php endif; ?>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
 
-<!-- Navbar -->
-<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-  <!-- Container wrapper -->
-  <div class="container">
-    <!-- Navbar brand -->
-    <a class="navbar-brand" href="#"><i class="fab fa-linkedin fa-2x"></i></a>
-    <!-- Search form -->
-    <form class="input-group" style="width: 400px">
-      <input type="search" class="form-control" placeholder="Type query" aria-label="Search" />
-      <button class="btn btn-outline-primary" type="button" data-mdb-ripple-color="dark" style="padding: .45rem 1.5rem .35rem;">
-        Search
-      </button>
-    </form>
-
-    <!-- Toggle button -->
-    <button class="navbar-toggler" type="button" data-mdb-toggle="collapse"
-      data-mdb-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-      aria-label="Toggle navigation">
-      <i class="fas fa-bars"></i>
-    </button>
-
-    <!-- Collapsible wrapper -->
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <!-- Left links -->
-      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active d-flex flex-column text-center" aria-current="page" href="#"><i class="fas fa-home fa-lg"></i><span class="small">Home</span></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link d-flex flex-column text-center" aria-current="page" href="#"><i class="fas fa-user-friends fa-lg"></i><span class="small">My Network</span></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link d-flex flex-column text-center" aria-current="page" href="#"><i class="fas fa-briefcase fa-lg"></i><span class="small">Jobs</span></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link d-flex flex-column text-center" aria-current="page" href="#"><i class="fas fa-comment-dots fa-lg"></i><span class="small">Messaging</span></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link d-flex flex-column text-center" aria-current="page" href="#"><i class="fas fa-bell fa-lg"></i><span class="small">Notifications</span></a>
-        </li>
-        <li class="nav-item dropdown">
-          <a
-            class="nav-link dropdown-toggle d-flex align-items-center"
-            href="#"
-            id="navbarDropdownMenuLink"
-            role="button"
-            data-mdb-toggle="dropdown"
-            aria-expanded="false"
-          >
-            <img
-              src="https://mdbootstrap.com/img/Photos/Avatars/img%20(9).jpg"
-              class="rounded-circle"
-              height="30"
-              alt=""
-              loading="lazy"
-            />
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <li><a class="dropdown-item" href="#">My profile</a></li>
-            <li><a class="dropdown-item" href="#">Settings</a></li>
-            <li><a class="dropdown-item" href="#">Logout</a></li>
-          </ul>
-        </li>
-      </ul>
-      <!-- Left links -->
-    </div>
-    <!-- Collapsible wrapper -->
-  </div>
-  <!-- Container wrapper -->
-</nav>
-<!-- Navbar -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
